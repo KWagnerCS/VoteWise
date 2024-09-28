179331268aa7f3c61f786c4bf20bba1add5fc084
alexanderaziz <110403151+alexanderaziz@users.noreply.github.com>
2024-09-28 14:09:08 -0400

    Router Init

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import './HomePage.css'
+
+function HomePage() {
+    return (
+        <>
+            <h1>Your voice matters. Let us help u express it or some shi.</h1>
+            <button>Get Started</button>
+        </>
+    )
+}
+
+export default HomePage

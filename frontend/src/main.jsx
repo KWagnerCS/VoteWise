a6ae508b4c5d645ceee91316c28f2a966c9e93d1
alexanderaziz <110403151+alexanderaziz@users.noreply.github.com>
2024-09-28 15:29:07 -0400

    delete vite css, add header

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App className="p-0"/>
   </StrictMode>,
 )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './HomePage.css'
+import '../index.css'
+import Header from '../components/Header'
 
 function HomePage() {
     return (
         <>
-            <h1>Your voice matters. Let us help u express it or some shi.</h1>
-            <button>Get Started</button>
+            <Header />
+            <h1 className="text-2xl p-5 font-bold">Your voice matters. Let us help u express it or some shi.</h1>
+            <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">Get Started</button>
         </>
     )
 }

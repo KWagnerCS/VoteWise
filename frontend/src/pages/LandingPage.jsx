6b9c10cda20c0bf53b427e2ae26a5131ccaaefe5
Kevin Wagner <111701655+KWagnerCS@users.noreply.github.com>
2024-10-01 00:24:15 -0400

    Update LandingPage.jsx
    funny

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -19,7 +19,7 @@ const LandingPage = () => {
             <div className="h-screen flex flex-col justify-center items-center bg-fixed bg-cover bg-center" style={{ backgroundImage: "url('/flagrealsky.jpg')" }}>
             </div>
             <div className="h-screen flex flex-col justify-center items-center bg-gray-200">
-                <h1 className="text-4xl mb-4 font-medium">Your Voice Matters—Let’s Help You Make It Heard!</h1>
+                <h1 className="text-4xl mb-4 font-medium">John Pork wants you to vote!</h1>
                 <button
                     onClick={() => scrollToSection('section2')}
                     className="px-6 py-2 text-lg bg-blue-500 text-white rounded hover:bg-blue-600"

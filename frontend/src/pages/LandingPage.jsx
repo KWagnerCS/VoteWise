adff0225bf9ca20d9da49552056917c3d2cfc6d3
Kevin Wagner <111701655+KWagnerCS@users.noreply.github.com>
2024-10-01 00:32:50 -0400

    Revert "Update LandingPage.jsx"
    This reverts commit 98041fa5e1f7e5b581769257016cc4e229f731a6.

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -19,7 +19,7 @@ const LandingPage = () => {
             <div className="h-screen flex flex-col justify-center items-center bg-fixed bg-cover bg-center" style={{ backgroundImage: "url('/flagrealsky.jpg')" }}>
             </div>
             <div className="h-screen flex flex-col justify-center items-center bg-gray-200">
-                <h1 className="text-4xl mb-4 font-medium"></h1>
+                <h1 className="text-4xl mb-4 font-medium">Your voice matters. Let us help u express it or some shi.</h1>
                 <button
                     onClick={() => scrollToSection('section2')}
                     className="px-6 py-2 text-lg bg-blue-500 text-white rounded hover:bg-blue-600"
